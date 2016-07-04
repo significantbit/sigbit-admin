d32f215be5f23a0652fd56b852a9a712202d599e
Ante Wall <ante@significantbit.se>
2016-07-04 09:35:29 +0200

    Inital structure

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('sass', function () {
+  return gulp.src('./public/**/*.scss')
+    .pipe(sass({
+      includePaths: require('node-bourbon').includePaths,
+      errLogToConsole: true
+    }).on('error', sass.logError))
+    .pipe(concat('style.css')) // this is what was missing
+    .pipe(gulp.dest('./public/css')); // output to theme root
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('nodemon', function () {
+  nodemon({
+    script: 'index.js',
+    watch: ['index.js']
+  }).on('start', function onStart() {
+    // ensure start only got called once
+    if (!called) { cb(); }
+    called = true;
+  }).on('restart', function onRestart() {
+    // reload connected browsers after a slight delay
+    setTimeout(function reload() {
+      browserSync.reload({
+        stream: false
+      });
+    }, BROWSER_SYNC_RELOAD_DELAY);
+  });
+});
+
+gulp.task('browser-sync', ['nodemon'], function () {
+
+  // for more browser-sync config options: http://www.browsersync.io/docs/options/
+  browserSync({
+
+    // informs browser-sync to proxy our expressjs app which would run at the following location
+    proxy: 'http://localhost:3000',
+
+    // informs browser-sync to use the following port for the proxied app
+    // notice that the default port is 3000, which would clash with our expressjs
+    port: 4000,
+
+    // open the proxied app in chrome
+    browser: ['google-chrome']
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch('public/**/*.js', ['js', browserSync.reload]);
+  gulp.watch('public/**/*.scss', ['sass', browserSync.reload]);
+  gulp.watch('public/**/*.html', ['bs-reload']);
+});
---
+++ index.js
@@ -0,0 +1,26 @@
+
+'use strict';
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
+var server = http.createServer(app);
+
+app.get('/', function(req, res) {
+
+  // Toggle between serving public/index.html
+  // and sending a text 'Ola Mundo!' to see
+  // nodemon restarting the server upon edit
+
+  res.sendFile('public/index.html', {"root": __dirname});
+//  res.send('Ola Mundo!');
+
+});
+
+app.use(express.static('public'));
+
+server.listen(3000, 'localhost');
+server.on('listening', function() {
+  console.log('Express server started on port %s at %s', server.address().port, server.address().address);
+});
