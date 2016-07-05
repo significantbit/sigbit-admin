93ce7d88fbeaf2bde4e884be46005a9b223f4993
Andreas Antonsson <hello@andreasantonsson.se>
2016-07-05 13:30:14 +0200

    Table buttons

--- public/scripts.js
+++ public/scripts.js
@@ -15137,3 +15137,13 @@ $("#sidebar-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+// Enable Tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Bootstrap button dropdown inside responsive table not visible because of scroll
+// http://stackoverflow.com/questions/26018756/bootstrap-button-drop-down-inside-responsive-table-not-visible-because-of-scroll
+$('body') .on('show.bs.dropdown', '.table-responsive', function () { $(this).css("overflow", "visible"); })
+.on('hide.bs.dropdown', '.table-responsive', function () { $(this).css("overflow", "auto"); });
