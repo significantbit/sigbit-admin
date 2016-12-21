8094fabffedb97b3c93f3c56bfc10ebc4b68b3ac
Andreas Antonsson <andreas@significantbit.se>
2016-12-21 16:14:46 +0100

    Remove path and link from nav

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ app.get('/', function(req, res) {
 app.get('/buttons', function(req, res) {
   res.render('buttons.ejs');
 });
-app.get('/actiontoolbar', function(req, res) {
-  res.render('actiontoolbar.ejs');
-});
 app.get('/colors', function(req, res) {
   res.render('colors.ejs');
 });
